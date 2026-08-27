a39ffd3ab64464ce156c7ddbad31e73b6f981086
ttinsley077-star <t.tinsley077@gmail.com>
2026-08-27 16:08:22 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE="tree-service-ops-v4";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
